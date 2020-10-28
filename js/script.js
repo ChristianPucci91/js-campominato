63f46bed28bb2872962f7de025828ae930989d85
ChristianPucci91 <pucci.christian91@gmail.com>
2020-10-28 19:24:43 +0100

    Funzione numeri random + inserimento input e condizioni

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// >>>>>>>>>> DESCRIZIONE ESERCIZIO <<<<<<<<<<<
+
 // STEP 1 Il computer deve generare 16 numeri casuali tra 1 e 100.
 // STEP 2 I numeri non possono essere duplicati
 // STEP 3 In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
@@ -6,11 +8,47 @@
 // STEP 6 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 //  STEP 7 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+// >>>>>>>>>> STEP PERSONALI <<<<<<<<<<<<<<<
+// var livello sarà la variabile che utilizzeremo per l'esercizio bonus ovvero il range di numeri con cui si può giocare, per ora la settiamo a 16.
+var livello = 10;
+var mine = 5;
+// creo variabile array per i numeri generati ( mine del gioco).
+var numeriGenerati = [];
+// richiamo la funzione con gli argomenti 1 e livello
+var generaMine = generaNumeri(1,livello)
+
 // STEP 1 devo generare 16 numeri casuali tra 1 e 100, potrei usare un ciclo con un math random e acquisire tutti e 16 i numeri dentro un array.
 
-// STEP 2 aggiungo una condizione che gli elementi dell'array non possono essere uguali tra loro
+// Creo una funzione per generare 16 numeri random da 1 a 100. dico al ciclo for di incrementare i fino a fare 16 cicli, e di stamparmi un numero mettendo una condizione while specificando con include(), che finchè non stampa un numero diverso dagli elementi dell'array non può uscire dalla condizione
+function generaNumeri(min,max) {
+
+  for (var i = 0; i < mine; i++) {
+    numeriGeneratiElemento = Math.floor(Math.random()* max +min);
+    // console.log(numeriGeneratiElemento);
+    while (numeriGenerati.includes(numeriGeneratiElemento)) {
+      numeriGeneratiElemento = Math.floor(Math.random()* max +min);
+    }
+    numeriGenerati.push(numeriGeneratiElemento);
+    // numeriGeneratiElemento += numeriGeneratiElemento + numeriGeneratiElemento;
+    // console.log("numeri dell'array",numeriGeneratiElemento);
+  }
+
+}
+console.log("l'array è composto da: ",numeriGenerati);
 
 // STEP 3 Chiedo con un prompt o input all'utente di inserire un numero compreso tra 1 e 100 per 84 volte, quindi ci sarà un ciclo con contatore a 84.
+var numeriInseriti = [];
+var partita = false;
+
+for (var x = 0; x < (livello-mine); x++) {
+  var inputUtente = parseInt(prompt("Inserisci un numero da 1 a " + livello));
+  while (numeriGenerati.includes(inputUtente)){
+    var inputUtente = parseInt(prompt("Numero già inserito, riprova"));
+  }
+  numeriInseriti.push(inputUtente);
+  console.log("i numeri inseriti ",numeriInseriti);
+}
+
 
 // STEP 4 il valore che andrà ad inserire l'utente andrà pushato all'interno di un array. Ogni volta che inserirà un valore nuovo, dovrò controllare con una condizione se il valore inserito sarà uguale agli elementi dell array.
 
